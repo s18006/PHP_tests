40017a26e82fe0b09f52b3577540d81d9ab8c6b0
Kecsa <s18006@std.it-college.ac.jp>
2019-07-04 00:44:44 +0900

    0704

--- quiz4/checkbox/index.php
+++ quiz4/checkbox/index.php
@@ -1,3 +1,7 @@
+<?php
+    require_once 'classes/startContent.php';
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -11,80 +15,11 @@
     </div>
     <form action="game.php" method="POST" onsubmit="return validation()">
       <div class="container">
-        <!-- 1st row -->
-        <div class="container-toggle1">
-          <input id="toggle1" value="1" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle1"> 情報セキュリティマネジメント</label>
-        </div>
-        <div class="container-toggle2">
-          <input id="toggle2" value="2" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle2">セキュリティ運用</label>
-        </div>
-        <div class="container-toggle3">
-          <input id="toggle3" value="3" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle3">インフラセキュリティ</label>
-        </div>
-        <!-- 2nd row -->
-        <div class="container-toggle4">
-          <input id="toggle4" value="4" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle4"> 不正アクセス</label>
-        </div>
-        <div class="container-toggle5">
-          <input id="toggle5" value="5" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle5"> ファイアウォール </label>
-        </div>
-        <div class="container-toggle6">
-          <input id="toggle6" value="6" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle6">侵入検知</label>
-        </div>
-        <!-- 3rd row-->
-        <div class="container-toggle7">
-          <input id="toggle7" value="7" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle7"> アプリケーションセキュリティ</label>
-        </div>
-        <div class="container-toggle8">
-          <input id="toggle8" value="8" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle8"> OSセキュリティ </label>
-        </div>
-        <div class="container-toggle9">
-          <input id="toggle9" value="9" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle9">認証</label>
-        </div>
-        <!-- 4th row -->
-        <div class="container-toggle10">
-          <input id="toggle10" value="10" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle10"> プログラミング</label>
-        </div>
-        <div class="container-toggle11">
-          <input id="toggle11" value="11" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle11"> 不正プログラム </label>
-        </div>
-        <div class="container-toggle12">
-          <input id="toggle12" value="12" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle12"> 暗号</label>
-        </div>
-        <!-- 5th row -->
-        <div class="container-toggle13">
-          <input id="toggle13" value="13" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle13"> 電子署名</label>
-        </div>
-        <div class="container-toggle14">
-          <input id="toggle14" value="14" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle14"> PKI </label>
-        </div>
-        <div class="container-toggle15">
-          <input id="toggle15" value="15" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle15"> セキュリティプロトコル</label>
-        </div>
-        <!-- 6th row -->
-        <div class="container-toggle16">
-          <input id="toggle16" value="16" type="checkbox" name="title[]" onclick="display(this)">
-          <label for="toggle16"> 法令・規格 </label>
-        </div>
+        <?php echo $checkboxElem; ?>
       </div>
       <div class="submit-container">
         <div class="summary-container">
-          <span id="summary" class="summary"> Total: 0</span>
+          <span id="summary" class="summary"> 選択した章: 0 問題の数: 0</span>
         </div>
         <div class="btn-container">
           <button type="submit" class="submit-btn">送信 </button>
@@ -92,5 +27,6 @@
       </div>
     </form>
     <input type="hidden" name="num" id="num" value="0"/>
+    <input type="hidden" name="gamelennum" id="gamelen" value="0"/>
   </body>
 </html>
--- quiz4/checkbox/js/index.js
+++ quiz4/checkbox/js/index.js
@@ -8,9 +8,13 @@ const clearcheckBox = () => {
 };
 
 const display = (obj) => {
+  //章の数を数える
   let sum = obj.checked === true ? parseInt(document.getElementById('num').value) + 1 : parseInt(document.getElementById('num').value) - 1;
   document.getElementById('num').value = sum;
-  document.getElementById('summary').innerHTML = 'Total: '+ sum.toString();
+  //問題の数を数える
+  let gamelen = obj.checked === true ? document.getElementById(obj.id+'-len').value : document.getElementById(obj.id + '-len').value * - 1;
+  document.getElementById('gamelen').value = parseInt(document.getElementById('gamelen').value) +  parseInt(gamelen);
+  document.getElementById('summary').innerHTML = '選択した章: '+ sum.toString() + '  問題の数: ' + document.getElementById('gamelen').value;
 }
 
 const validation = () => {
