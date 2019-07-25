1ecc8651dd84464e43792b808999ceaf3124ed02
Kecsa <s18006@std.it-college.ac.jp>
2019-07-26 01:36:48 +0900

    Iterator

--- Iterator/Main.php
+++ Iterator/Main.php
@@ -1,13 +1,17 @@
 <?php
 require_once 'BookShelf.php';
-
-$bookShelf = new BookShelf(4);
-$bookShelf -> appendBook('Around the World in 80 Days');
-$bookShelf -> appendBook('Bible');
-$bookShelf -> appendBook('Cindarella');
-$bookShelf -> appendBook('Daddy-Long-Legs');
-$bookShelf -> iterator();
-while($bookShelf -> hasNext()) {
-    echo $bookShelf -> next(). "\n";
+class Main {
+    public function __construct() {
+        $bookShelf = new BookShelf(4);
+        $bookShelf -> appendBook('Around the World in 80 Days');
+        $bookShelf -> appendBook('Bible');
+        $bookShelf -> appendBook('Cindarella');
+        $bookShelf -> appendBook('Daddy-Long-Legs');
+        $bookShelf -> iterator();
+        while($bookShelf -> hasNext()) {
+            echo $bookShelf -> next(). "\n";
+        }
+    }
 }
+$main = new Main();
 ?>
