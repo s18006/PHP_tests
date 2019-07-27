fe8030a57e6f35d1bb3339cc643863b3ecda93cc
Kecsa <s18006@std.it-college.ac.jp>
2019-07-27 22:15:01 +0900

    Iterator

---
+++ Iterator/Book.php
@@ -0,0 +1,14 @@
+<?php
+
+class Book {
+    private $name;
+
+    public function Book($name) {
+        $this -> name = $name;
+    }
+
+    public function getName() {
+        return $this -> name;
+    }
+}
+?>
--- Iterator/Main.php
+++ Iterator/Main.php
@@ -1,17 +1,18 @@
 <?php
+require_once 'Book.php';
 require_once 'BookShelf.php';
 require_once 'BookShelfIterator.php';
 
 class Main {
     public function __construct() {
         $bookShelf = new BookShelf(4);
-        $bookShelf -> appendBook('Around the World in 80 Days');
-        $bookShelf -> appendBook('Bible');
-        $bookShelf -> appendBook('Cindarella');
-        $bookShelf -> appendBook('Daddy-Long-Legs');
+        $bookShelf -> appendBook(new Book('Around the World in 80 Days'));
+        $bookShelf -> appendBook(new Book('Bible'));
+        $bookShelf -> appendBook(new Book('Cindarella'));
+        $bookShelf -> appendBook(new Book('Daddy-Long-Legs'));
         $it = $bookShelf -> iterator();
         while($it -> hasNext()) {
-            echo $it -> next(). "\n";
+            echo $it -> next() -> getName(). "\n";
         }
     }
 }
