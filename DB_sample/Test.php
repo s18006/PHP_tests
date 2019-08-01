a049b45d24b4430635b001a911cc48151428287d
Kecsa <s18006@std.it-college.ac.jp>
2019-08-02 08:55:52 +0900

    DB_sample

--- DB_sample/Test.php
+++ DB_sample/Test.php
@@ -4,18 +4,29 @@ require_once 'classes/dbManagerClass.php';
 $db = new dbManager;
 
 $query = array('type' => 'fetchWithColName',
-                'query' => "SELECT * FROM users WHERE id=?",
-                'idxList' => array(1),
+                'query' => "SELECT * FROM users",
+                'parameter' => '',
                 );
-
-print_r($db -> dbMethod('download', $query)) . PHP_EOL;
+print_r($db -> dbMethod($query)) . PHP_EOL;
 
 $query = array('type' => 'fetchWithoutColName',
                 'query' => "SELECT COUNT(*) FROM users",
-                'idxList' => array(),
+                'parameter' => '',
                 );
 
-print_r($db -> dbMethod('download', $query)) . PHP_EOL;
+print_r($db -> dbMethod($query)) . PHP_EOL;
+
+$query = array('query' => "INSERT INTO users (id, username, password) VALUES (?, ?, ?)",
+                'parameter' => array(5, 'Tom', '1234'),
+                );
+
+echo ($db -> dbMethod($query)) . PHP_EOL;
+
+$query = array('query' => "DELETE FROM users WHERE id=?",
+                'parameter' => 5,
+                );
+
+echo ($db -> dbMethod($query)) . PHP_EOL;
 
 
 
--- DB_sample/classes/dbManagerClass.php
+++ DB_sample/classes/dbManagerClass.php
@@ -1,24 +1,50 @@
 <?php
 
 interface Manager {
-    public function dbMethod($type, $query);
+    public function dbMethod($query);
+    public static function queryAnalyzer(string $query):string;
 }
 
 
 class dbManager implements Manager {
+
     //type can be: download, upload, delete
-    public function dbMethod($type, $query) {
-        $result = new $type($query);
+    public function dbMethod($query) {
+        //query needs to inclue query and parameter element
+        //change parameters to array
+        if (!is_array($query['parameter'])) {
+               $query['parameter'] = array($query['parameter']);
+        }
+        $queryType = self::queryAnalyzer($query['query']);
+        $result = new $queryType($query);
         return $result -> getResult();
     }
-}
 
+    public static function queryAnalyzer(string $query):string {
+        //define type of query
+        $type;
+        $queryLine = explode (' ', $query);
+        switch(strtolower($queryLine[0])) {
+            case 'select':
+                $type = 'download';
+                break;
+            case 'delete':
+                $type = 'delete';
+                break;
+            case 'insert':
+            case 'update':
+                $type = 'upload';
+                break;
+        }
+        return $type;
+    }
+}
 
 abstract class connection {
 
     protected $db;
     protected $query;
-    protected $idxList;
+    protected $parameter;
 
     //PDO connection
     public function __construct($query) {
@@ -28,32 +54,34 @@ abstract class connection {
             exit("データベースに接続できません。: {$e->getMessage()}");
         }
         $this -> query = $query['query'];
-        $this -> idxList = $query['idxList'];
+        $this -> parameter = $query['parameter'];
     }
 
-    public abstract function prepareStat():object;
+    public abstract function prepareStat();
 }
 
 class download extends connection {
 
-    private $type;
+    private $type = 'fetchWithColName';
     private $result;
 
     public function __construct($query) {
         parent::__construct($query);
-        $this -> type = $query['type'];
+        if (isset($query['type'])) {
+            $this -> type = $query['type'];
+        }
         $this -> {$this -> type}();
     }
 
     public function prepareStat():object {
         $stt = $this -> db -> prepare($this -> query);
-        $stt->execute($this -> idxList);
+        $stt->execute($this -> parameter);
         return $stt;
     }
 
     public function fetchWithColName():void {
         $stt = $this -> prepareStat();
-        $this -> result = $stt->fetch(PDO::FETCH_ASSOC);
+        $this -> result = $stt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function fetchWithoutColName():void {
@@ -66,20 +94,46 @@ class download extends connection {
     }
 }
 
-class upload extends connection {
+abstract class oneway extends connection {
+
+    protected $result;
 
     public function __construct($query) {
-        parent__construct();
+        parent::__construct($query);
     }
 
-    public function prepareStat():object {
-        return $this;
+    public function prepareStat():void {
+        $stt = $this -> db -> prepare($this -> query);
+        $stt->execute($this -> parameter);
+        $this -> result = true;
     }
 
+    public abstract function getResult():string;
 }
 
+class upload extends oneway {
 
+    public function __construct($query) {
+        parent::__construct($query);
+        $this -> prepareStat();
+    }
 
+    public function getResult():string {
+        $result = ($this -> result === true) ? 'Upload was successful' : 'Upload was not successful';
+        return $result;
+    }
+}
 
+class delete extends oneway {
 
+    public function __construct($query) {
+        parent::__construct($query);
+        $this -> prepareStat();
+    }
+
+    public function getResult():string {
+        $result = ($this -> result === true) ? 'Object is deleted' : 'Object is not deleted';
+        return $result;
+    }
+}
 ?>
