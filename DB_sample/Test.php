200c671b699a65d71999d65ec41c37f2efec3b9d
Kecsa <s18006@std.it-college.ac.jp>
2019-08-01 21:46:00 +0900

    0801

--- DB_sample/Test.php
+++ DB_sample/Test.php
@@ -3,18 +3,17 @@
 require_once 'classes/dbManagerClass.php';
 $db = new dbManager;
 
-$query = array('type' => 'oneRowAll',
+$query = array('type' => 'fetchWithColName',
                 'query' => "SELECT * FROM users WHERE id=?",
-                'idxList' => 1,
-                'paramList' => 'i');
+                'idxList' => array(1),
+                );
 
 print_r($db -> dbMethod('download', $query)) . PHP_EOL;
 
-$query = array('type' => 'allData',
-                'query' => "SELECT id, username, password FROM users",
-                'idxList' => '',
-                'paramList' => '',
-                'columnLen' => 3);
+$query = array('type' => 'fetchWithoutColName',
+                'query' => "SELECT COUNT(*) FROM users",
+                'idxList' => array(),
+                );
 
 print_r($db -> dbMethod('download', $query)) . PHP_EOL;
 
--- DB_sample/classes/dbManagerClass.php
+++ DB_sample/classes/dbManagerClass.php
@@ -17,97 +17,69 @@ class dbManager implements Manager {
 abstract class connection {
 
     protected $db;
+    protected $query;
+    protected $idxList;
 
     //PDO connection
-    public function __construct() {
+    public function __construct($query) {
         try {
             $this -> db = new PDO('mysql:dbname=newtables;host=localhost;charset=utf8', 'testuser', '0808');
         } catch (PDOException $e) {
             exit("データベースに接続できません。: {$e->getMessage()}");
         }
+        $this -> query = $query['query'];
+        $this -> idxList = $query['idxList'];
     }
-}
 
+    public abstract function prepareStat():object;
+}
 
 class download extends connection {
 
     private $type;
     private $result;
-    private $query;
-    private $idxList;
-    private $paramList = '';
-    private $columnLen = 0;
-    //if we fill columns with content
-    private $columnTempContent = array();
 
     public function __construct($query) {
-        parent::__construct();
+        parent::__construct($query);
         $this -> type = $query['type'];
-        $this -> query = $query['query'];
-        $this -> idxList = $query['idxList'];
-        $this -> paramList = $query['paramList'];
-        if (isset($query['columnLen'])) {
-            $this -> columnLen = $query['columnLen'];
-        }
         $this -> {$this -> type}();
     }
 
     public function prepareStat():object {
         $stt = $this -> db -> prepare($this -> query);
-        if (strlen($this -> paramList) > 1) {
-            //if query statement includes more than one parameter
-            for ($i = 0; $i < strlen($this-> paramList); $i++) {
-                $paramIdx = $i + 1;
-                if ($this -> paramList[$i] === 'i') {
-                    $stt->bindParam($paramIdx, $this -> idxList[$i], PDO::PARAM_INT);
-                } else {
-                    $stt->bindParam($paramIdx, $this -> idxList[$i], PDO::PARAM_STR);
-                }
-            }
-        }
-        if (strlen($this -> paramList) === 1) {
-            if ($this -> paramList === 'i') {
-                $stt->bindParam(1, $this -> idxList, PDO::PARAM_INT);
-            } else {
-                $stt->bindParam(1, $this -> idxList, PDO::PARAM_STR);
-            }
-        }
-        $stt->execute();
+        $stt->execute($this -> idxList);
         return $stt;
     }
 
-
-    public function fillColumnContent($stt):object {
-        for ($i = 0; $i < $this -> columnLen; $i++) {
-            $param_idx = $i + 1;
-            $stt-> bindColumn($param_idx, $this -> columnTempContent[$i]);
-        }
-        return $stt;
-    }
-
-    //query with *
-    public function oneRowAll():void {
+    public function fetchWithColName():void {
         $stt = $this -> prepareStat();
         $this -> result = $stt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function allData():void {
-        $prep_stt = $this -> prepareStat();
-        $stt = $this -> fillColumnContent($prep_stt);
-        $resultIdx = 0;
-        $result = array();
-        while($row = $stt->fetch()) {
-            for ($k = 0; $k < $this -> columnLen; $k++) {
-                $result[$resultIdx][$k] = $this -> columnTempContent[$k];
-            }
-            $resultIdx++;
-        };
-        $this -> result = $result;
+    public function fetchWithoutColName():void {
+        $stt = $this -> prepareStat();
+        $this -> result = $stt -> fetchAll(PDO::FETCH_COLUMN);
     }
 
     public function getResult() {
         return $this -> result;
     }
 }
+
+class upload extends connection {
+
+    public function __construct($query) {
+        parent__construct();
+    }
+
+    public function prepareStat():object {
+        return $this;
+    }
+
+}
+
+
+
+
 
 ?>
