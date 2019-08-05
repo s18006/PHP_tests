bdf4952302da4bda4ce8df6981b30755c7c7f814
Kecsa <s18006@std.it-college.ac.jp>
2019-08-05 13:24:53 +0900

    final ver

--- DB_sample/Test.php
+++ DB_sample/Test.php
@@ -19,22 +19,33 @@ require_once 'classes/CheckboxCreator.php';
 $db = new dbManager;
 
 $query = array('type' => 'fetchWithColName',
-                'query' => "SELECT id as ID, username as ユーザー名, email as メールアドレス FROM users",
-                'parameter' => '',
+               'query' => "SELECT id as ID, username as ユーザー名, email as メールアドレス FROM users",
+               'parameter' => '',
                 );
 $items = $db -> dbMethod($query);
+/* first step */
+//print_r($items);
 
-$query = array('type' => 'fetchWithColName',
-                'query' => "SELECT t.click, u.password from users u, (SELECT 'パスワードを表示して' click from dual) t",
-                'parameter' => "",
-            );
-$rb_cont = $db -> dbMethod($query);
-$rb = new RadioButtonCreator;
-$rb -> add($rb_cont);
-$items2 = $rb -> createAsArray('パスワードの表示');
 
+/* second step: create simple table element */
 $table = new tableCreator('ユーザー');
 $table -> add($items);
+
+
+$query = array('type' => 'fetchWithColName',
+               'query' => "SELECT t.click, u.password from users u, (SELECT 'パスワードを表示して' click from dual) t",
+               'parameter' => "",
+            );
+$rb_cont = $db -> dbMethod($query);
+
+$rb = new RadioButtonCreator;
+$rb -> add($rb_cont);
+//$rbElem = $rb -> createAsString();
+/*third step: create simple radio button elements */
+//echo $rbElem;
+
+/*forth step: add radio button elements to table content */
+$items2 = $rb -> createAsArray('パスワードの表示');
 $table -> add($items2);
 echo $table -> create(new tableBody());
 
